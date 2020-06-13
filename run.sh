94f737619f9a9f92e3a91fab26e26bc049c1da85
Nova Herdi Kusumah <novaherdi@gmail.com>
2020-06-13 11:42:03 +0700

    change mongo docker source

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ docker rm $DOCKER_NAME
 docker rmi $DOCKER_NAME
 
 docker run --name redis -d -p 6379:6379 redis redis-server --appendonly yes
-docker run -itd -p 27017:27017 --name mongo -v /c/docker/indoquran-golang/docker/mongo:/data/db mongo
+docker run -itd -p 27017:27017 --name mongo -v /c/Temp/data/docker/mongo:/bitnami/mongodb bitnami/mongodb
 docker build --build-arg ENV=development . -t $DOCKER_NAME
 docker run -it -d -p 8080:8080 --env ENV=development --name $DOCKER_NAME --link mongo --link redis $DOCKER_NAME
 
