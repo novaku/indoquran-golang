38c391bd299fc860287aa0730d9035a9b21db741
Nova Herdi Kusumah <novaherdi@gmail.com>
2020-06-13 18:34:51 +0700

    glog example

--- routes/api.go
+++ routes/api.go
@@ -14,7 +14,9 @@ func APIRoutes(r *gin.Engine) {
 	{
 		v1.GET("/ping", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"message": "pong kaleee"})
-			glog.Info("only ping")
+			glog.Infof("cuman test %s adalah %s", "aplikasi", "indoquran")
+			glog.Warningf("test warning %s", "test lagi")
+			glog.Errorf("test error %s", "lagi")
 		})
 
 		user := new(controllers.UserController)
